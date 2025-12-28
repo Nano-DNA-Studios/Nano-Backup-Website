8be554245c4bf3c3bd7740c099b59ae793aeb396
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-12-28 16:02:54 -0500

    Added Error Redirect to Download Service
    - Added an Error redirect to the Download Service

--- Nano-Backup-Website/DownloadController.cs
+++ Nano-Backup-Website/DownloadController.cs
@@ -8,14 +8,14 @@ namespace NanoBackupWebsite
     public class DownloadController : ControllerBase
     {
         [HttpGet]
-        public IActionResult GetFile([FromQuery] string fullPath, [FromQuery] string fileName)
+        public IActionResult GetFile([FromQuery] string fullPath, [FromQuery] string fileName, [FromQuery] string returnUrl)
         {
             // Get the Path to the File on the local machine
             string baseFolder = AppContext.BaseDirectory;
             string path = Path.Combine(baseFolder, fullPath, fileName);
 
             if (!System.IO.File.Exists(path))
-                return NoContent();
+                return Redirect($"{returnUrl}?error=NotFound");
 
             FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
 
