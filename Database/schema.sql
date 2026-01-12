4764af9bde5e5ebcd512fca57c7bc7e8c966544c
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-01-11 19:44:51 -0500

    Updated the Schema of the Database
    - Updated the Schema of the database to handle the new strategy

--- Database/schema.sql
+++ Database/schema.sql
@@ -2,8 +2,9 @@ CREATE TABLE nanobackupdatabase(
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
 	is_file BOOLEAN NOT NULL,
+	is_7z BOOLEAN NOT NULL,
 	size_bytes BIGINT DEFAULT 0,
 	path TEXT,
 	parent_id INTEGER REFERENCES nanobackupdatabase(id),
-	file_data OID
+	parent_7z INTEGER REFERENCES nanobackupdatabase(id),
 );
