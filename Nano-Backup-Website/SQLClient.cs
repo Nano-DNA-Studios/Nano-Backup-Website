ad9459579f9edefae19c19dbafbc40974137e57e
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-01-15 22:10:51 -0500

    Added NPGSql support to the App and Created a client
    - Added Postgres SQL Support to the site
    - Created a Test Client file for communicating to the Database
    - Tested it out and it works

---
+++ Nano-Backup-Website/SQLClient.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+
+namespace NanoBackupWebsite
+{
+    public class SQLClient
+    {
+        private static string Host = "Host=localhost;";
+
+        private static string Port = "Port=5433;";
+
+        private static string Database = "Database=nanobackupwebsite;";
+
+        private static string Username = "Username=postgres;";
+
+        private static string Password = $"Password={Environment.GetEnvironmentVariable("POSTGRESPASSWORD") ?? ""};";
+
+        private static string ConnectionString = "";
+
+        public SQLClient ()
+        {
+            ConnectionString += Host;
+            ConnectionString += Port;
+            ConnectionString += Database;
+            ConnectionString += Username;
+            ConnectionString += Password;
+        }
+
+        public string[] GetFiles(int parentID)
+        {
+            string SQLQuery = "SELECT * FROM nanobackupdatabase WHERE parent_id = @id";
+
+            List<string> Files = new List<string>();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionString))
+            {
+                NpgsqlCommand command = new NpgsqlCommand(SQLQuery, connection);
+
+                command.Parameters.AddWithValue("@id", parentID);
+
+                try
+                {
+                    connection.Open();
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.ToString());
+                        }
+                           // Files.Add((string)reader["name"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+
+            return Files.ToArray();
+        }
+    }
+}
