0f8fdf8cc547d9540e5b75127b479070e04a3c8e
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-01-18 17:26:13 -0500

    Added Automatic Database Dump
    - Added the Ability to do a Automatic Database Dump
    - Tested it on the full database

--- DatabaseInjector/Program.cs
+++ DatabaseInjector/Program.cs
@@ -1,8 +1,11 @@
-﻿namespace DatabaseInjector
+﻿using NanoDNA.ProcessRunner;
+
+namespace DatabaseInjector
 {
     internal class Program
     {
-        private static string DatabasePath = @"E:\Backups\Important Backup\School\Class Backup SQL Test";
+        //private static string DatabasePath = @"E:\Backups\Important Backup\School\Class Backup SQL Test";
+        private static string DatabasePath = @"E:\Backups\Important Backup\School\University Class Backups";
 
         static void Main(string[] args)
         {
@@ -22,10 +25,15 @@
                 client.UpdateDirectorySizes();
 
                 Console.WriteLine("Finished Writing to SQL Database");
-           } catch (Exception e)
-           {
-               Console.WriteLine($"Error : {e.ToString()}");
-           }
+
+                Console.WriteLine("Exporting Database to SQL File...");
+
+                new CommandRunner().Run($"pg_dump -h localhost -p {Environment.GetEnvironmentVariable("Port")} -U {Environment.GetEnvironmentVariable("Username")} -d {Environment.GetEnvironmentVariable("Database")} > nanobackupwebsite.sql");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error : {e.ToString()}");
+            }
         }
 
         private static void LoadEnv()
