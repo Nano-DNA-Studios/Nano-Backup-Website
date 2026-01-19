60df563115a288a6acb29f0f96cb9f79dfb8c021
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-01-19 00:22:43 -0500

    Introduced a way to dispose of the Download Controller Stream
    - Found a way to dispose of the Download Controller Stream

--- Nano-Backup-Website/DownloadController.cs
+++ Nano-Backup-Website/DownloadController.cs
@@ -5,19 +5,30 @@ namespace NanoBackupWebsite
     // Set the URL at which files can be downloaded
     [ApiController]
     [Route("api/download")]
-    public class DownloadController : ControllerBase
+    public class DownloadController : ControllerBase, IDisposable
     {
+        Stream? Stream;
+
         [HttpGet]
         public IActionResult GetFile([FromQuery] string id, [FromQuery] string fileName, [FromQuery] string returnUrl)
         {
             Console.WriteLine($"Downloading File : {fileName}");
 
-            Stream? stream = new SQLClient().GetFileStream(int.Parse(id));
+            Stream = new SQLClient().GetFileStream(int.Parse(id));
 
-            if (stream == null)
+            if (Stream == null)
                 return Redirect($"{returnUrl}?error=NotFound");
 
-            return File(stream, "application/octet-stream", fileName, true);
+            return File(Stream, "application/octet-stream", fileName, true);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Download Controller Disposed");
+
+            Stream?.Dispose();
+
+            GC.Collect(3, GCCollectionMode.Forced, true);
         }
     }
 }
